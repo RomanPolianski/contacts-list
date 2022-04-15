d8c6e0d1e92a76e94a9d4561c8a1f96cba6a3191
Roman <rpol123453@gmail.com>
2022-04-15 17:06:22 +0300

    Create getContacts thunk

--- src/store/contactsSlice.js
+++ src/store/contactsSlice.js
@@ -1,4 +1,19 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import ContactsService from '../services/contactsService';
+
+export const fetchContacts = createAsyncThunk(
+  'contacts/fetchContacts',
+  async (_, { dispatch }) => {
+    try {
+      const response = await ContactsService.getContacts();
+      if (response.status === 200) {
+        dispatch(getContacts(response.data));
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
 
 const contactsSlice = createSlice({
   name: 'contacts',
@@ -7,7 +22,7 @@ const contactsSlice = createSlice({
   },
   reducers: {
     getContacts(state, action) {
-      state.projects = action.payload.contacts;
+      state.contacts = action.payload;
     },
   },
 });
