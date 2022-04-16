00867724bc51ccf1b0aedd011ffd764f73c5e0ff
Roman <rpol123453@gmail.com>
2022-04-16 14:11:34 +0300

    Refactor Table component

--- src/Components/Content/Table/Table.jsx
+++ src/Components/Content/Table/Table.jsx
@@ -1,35 +1,52 @@
+import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { NavLink } from 'react-router-dom';
 import s from './Table.module.css';
+import Task from './Task/Task';
+import Contact from './Contact/Contact';
+import { fetchContacts } from '../../../store/contactsSlice';
 
-const Table = ({ data, column }) => {
+const Table = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, []);
+
+  const contacts = useSelector((state) => state.contacts.contacts);
+  const c = contacts.map((p) => (
+    <Contact
+      key={p.contact_id}
+      contactId={p.contact_id}
+      name={p.name}
+      last_name={p.last_name}
+      company={p.company}
+      phone={p.phone}
+      email={p.email}
+      adress={p.adress}
+      operator={p.operator}
+      os={p.os}
+      tasks={p.tasks}
+    />
+  ));
+
   return (
-    <table>
-      <thead>
+    <table className={s.contentTable}>
+      <thead >
         <tr>
-          {column.map((item, index) => {
-            return <TableHeadItem item={item} key={index} />;
-          })}
+          <th>Name</th>
+          <th>Last Name</th>
+          <th>Company</th>
+          <th>Phone</th>
+          <th>Email</th>
+          <th>Adress</th>
+          <th>Operator</th>
+          <th>Os</th>
+          <th>Tasks</th>
+          <th> </th>
         </tr>
       </thead>
-      <tbody>
-        {data.map((item, index) => {
-          return <TableRow item={item} column={column} key={index} />;
-        })}
-      </tbody>
+      <tbody>{c}</tbody>
     </table>
-  );
-};
-
-const TableHeadItem = ({ item }) => {
-  return <th>{item.heading}</th>;
-};
-
-const TableRow = ({ item, column }) => {
-  return (
-    <tr>
-      {column.map((columnItem, index) => {
-        return <td>{item[`${columnItem.value}`]}</td>;
-      })}
-    </tr>
   );
 };
 
