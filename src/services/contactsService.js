a98e11b2f4cd53abdc0741f1fce90dd651ec96bf
Roman <rpol123453@gmail.com>
2022-04-16 22:26:22 +0300

    Add send and update contacts requests

--- src/services/contactsService.js
+++ src/services/contactsService.js
@@ -1,7 +1,59 @@
-import instance from "../axios/Api";
+import instance from '../axios/Api';
 
 export default class ContactsService {
   static async getContacts() {
     return instance.get('/contacts');
+  }
+
+  static async sendContact(
+    id,
+    name,
+    lastName,
+    company,
+    phone,
+    email,
+    adress,
+    operator,
+    os,
+    tasksUser
+  ) {
+    return instance.post('/create-contact', {
+      id,
+      name,
+      lastName,
+      company,
+      phone,
+      email,
+      adress,
+      operator,
+      os,
+      tasksUser,
+    });
+  }
+
+  static async sendUpdateContact(
+    id,
+    name,
+    lastName,
+    company,
+    phone,
+    email,
+    adress,
+    operator,
+    os,
+    tasksUser
+  ) {
+    return instance.put('/update-contact', {
+      id,
+      name,
+      lastName,
+      company,
+      phone,
+      email,
+      adress,
+      operator,
+      os,
+      tasksUser,
+    });
   }
 }
