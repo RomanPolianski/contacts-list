ea29e38a77cdcc879d8ce3d086a1eb01e97926cb
Roman <rpol123453@gmail.com>
2022-04-16 22:25:02 +0300

    Add send and update thunks

--- src/store/contactsSlice.js
+++ src/store/contactsSlice.js
@@ -1,13 +1,62 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import ContactsService from '../services/contactsService';
 
-export const fetchContacts = createAsyncThunk(
-  'contacts/fetchContacts',
-  async (_, { dispatch }) => {
+export const fetchContacts = createAsyncThunk('contacts/fetchContacts', async (_, { dispatch }) => {
+  try {
+    const response = await ContactsService.getContacts();
+    if (response.status === 200) {
+      dispatch(getContacts(response.data));
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+export const sendNewContact = createAsyncThunk(
+  'contacts/sendNewContact',
+  async (data, { dispatch }) => {
     try {
-      const response = await ContactsService.getContacts();
+      const { id, name, lastName, company, phone, email, adress, operator, os, tasksUser } = data;
+      const response = await ContactsService.sendContact(
+        id,
+        name,
+        lastName,
+        company,
+        phone,
+        email,
+        adress,
+        operator,
+        os,
+        tasksUser
+      );
       if (response.status === 200) {
-        dispatch(getContacts(response.data));
+        dispatch(createContactSuccess(response.data));
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
+
+export const sendUpdateContact = createAsyncThunk(
+  'contacts/sendUpdateContact',
+  async (data, { dispatch }) => {
+    try {
+      const { id, name, lastName, company, phone, email, adress, operator, os, tasksUser } = data;
+      const response = await ContactsService.sendUpdateContact(
+        id,
+        name,
+        lastName,
+        company,
+        phone,
+        email,
+        adress,
+        operator,
+        os,
+        tasksUser
+      );
+      if (response.status === 200) {
+        dispatch(createContactSuccess(response.data));
       }
     } catch (error) {
       console.log(error);
@@ -18,15 +67,22 @@ export const fetchContacts = createAsyncThunk(
 const contactsSlice = createSlice({
   name: 'contacts',
   initialState: {
+    contactId: null,
     contacts: [],
   },
   reducers: {
     getContacts(state, action) {
       state.contacts = action.payload;
     },
+    createContactSuccess(state, action) {
+      state.message = action.payload;
+    },
+    setContactId(state, action) {
+      state.contactId = action.payload;
+    },
   },
 });
 
-export const { getContacts } = contactsSlice.actions;
+export const { getContacts, createContactSuccess, setContactId } = contactsSlice.actions;
 
 export default contactsSlice.reducer;
