99f67eb19cb515eb507b8ffac8fde16f811fc497
Roman <rpol123453@gmail.com>
2022-04-17 13:01:29 +0300

    Move validation to scheme.js

--- src/Components/CreatePage/CreatePage.jsx
+++ src/Components/CreatePage/CreatePage.jsx
@@ -1,4 +1,3 @@
-import * as Yup from 'yup';
 import { Formik, Form, FieldArray } from 'formik';
 import InputField from '../common/InputField/InputField';
 import s from './CreatePage.module.css';
@@ -7,32 +6,9 @@ import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { sendNewContact } from '../../store/contactsSlice';
+import { scheme } from '../../validators/scheme';
 
 const CreatePage = () => {
-  const digitsOnly = (value) => /^\d+$/.test(value);
-  const validate = Yup.object({
-    name: Yup.string()
-      .required('Name Required')
-      .min(4, 'Name needs to be at least 4 characters')
-      .max(25, 'Name is too long'),
-    lastName: Yup.string()
-      .required('LastName Required')
-      .min(2, 'Last Name needs to be at least 2 characters')
-      .max(25, 'Last Name is too long'),
-    company: Yup.string()
-      .required('Company Name Required')
-      .min(2, 'Company Name needs to be at least 2 characters')
-      .max(25, 'Compay Name is too long'),
-    phone: Yup.string()
-      .required('Phone Required')
-      .test('Digits only', 'The field should have digits only', digitsOnly)
-      .max(9, 'Number is too long'),
-    email: Yup.string().email('Must be a valid email').max(25).required('Email is required'),
-    adress: Yup.string().required('Adress Required'),
-    operator: Yup.string().required('Operator Required'),
-    os: Yup.string().required('OS Required'),
-  });
-
   const dropDownOptions = [
     { key: 'Choose operator', value: '' },
     { key: 'MTS', value: 'MTS' },
@@ -67,7 +43,7 @@ const CreatePage = () => {
           },
         ],
       }}
-      validationSchema={validate}
+      validationSchema={scheme}
       onSubmit={(values) => {
         dispatch(sendNewContact(values));
       }}
---
+++ src/validators/scheme.js
@@ -0,0 +1,25 @@
+import * as Yup from 'yup';
+
+const digitsOnly = (value) => /^\d+$/.test(value);
+export const scheme = Yup.object({
+  name: Yup.string()
+    .required('Name Required')
+    .min(4, 'Name needs to be at least 4 characters')
+    .max(25, 'Name is too long'),
+  lastName: Yup.string()
+    .required('LastName Required')
+    .min(2, 'Last Name needs to be at least 2 characters')
+    .max(25, 'Last Name is too long'),
+  company: Yup.string()
+    .required('Company Name Required')
+    .min(2, 'Company Name needs to be at least 2 characters')
+    .max(25, 'Compay Name is too long'),
+  phone: Yup.string()
+    .required('Phone Required')
+    .test('Digits only', 'The field should have digits only', digitsOnly)
+    .max(9, 'Number is too long'),
+  email: Yup.string().email('Must be a valid email').max(25).required('Email is required'),
+  adress: Yup.string().required('Adress Required'),
+  operator: Yup.string().required('Operator Required'),
+  os: Yup.string().required('OS Required'),
+});
